bc269b7557fc7cce8cc55031c6f782c4b9d5c906
Marko <marko@startmail.com>
2020-06-03 03:44:41 +0200

    fix token service issue

--- pkg/handlers/me.go
+++ pkg/handlers/me.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 
 	"github.com/jinzhu/gorm"
+	"github.com/spf13/viper"
 	"gitlab.com/markomalis/packback-api/pkg/repositories"
+	"gitlab.com/markomalis/packback-api/pkg/services"
 )
 
 type MeHandler struct {
@@ -20,6 +22,19 @@ func (h MeHandler) GetMe(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
+
+	ts := services.NewTokenService(
+		viper.GetString("token_service.secret_key"),
+		viper.GetString("token_service.issuer"),
+		viper.GetInt("token_service.minutes_valid"),
+	)
+	token, err := ts.Encode(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	user.Token = token
 
 	writeSuccesResponse(user, w)
 }
