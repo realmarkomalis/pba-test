d09eee8de962bddf20f82a6aeb452988e5073b49
Marko <marko@startmail.com>
2020-10-30 11:38:12 +0100

    unify API errors

---
+++ pkg/entities/errors.go
@@ -0,0 +1,13 @@
+package entities
+
+import "fmt"
+
+type APIError struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+	Link    string `json:"link"`
+}
+
+func (e *APIError) Error() string {
+	return fmt.Sprintf("API Error: %s, for more info visit: %s", e.Message, e.Link)
+}
